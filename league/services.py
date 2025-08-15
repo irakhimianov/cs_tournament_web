8be40bce4baab571368ad08a3ca7884519d155fd
Ilgiz Rakhimianov <i@rakhimianov.ru>
2025-08-15 19:34:29 +0500

    Edit

--- league/services.py
+++ league/services.py
@@ -22,7 +22,12 @@ def generate_invite_code(length: int = 6) -> str:
 def notify_player_verification(instance: models.Player, old_instance: Optional[models.Player] = None) -> None:
     if instance.verified_at and (not old_instance or old_instance.verified_at is None):
         text = 'Ваша заявка одобрена!'
-        return transaction.on_commit(lambda: tasks.send_telegram_message.delay(instance.pk, text))
+        reply_markup = {
+            'inline_keyboard': [
+                [{'text': 'Меню', 'callback_data': 'menu'}],
+            ],
+        }
+        return transaction.on_commit(lambda: tasks.send_telegram_message.delay(instance.pk, text, reply_markup))
 
     if instance.verified_at is None and (not old_instance or old_instance.verified_at):
         text = 'Ваша заявка отозвана!'
@@ -35,9 +40,11 @@ class TelegramHandler:
     def __init__(self, token: str = settings.BOT_TOKEN) -> None:
         self.token = token
 
-    def send_message(self, chat_id: int, text: str) -> requests.Response:
+    def send_message(self, chat_id: int, text: str, reply_markup: Optional[dict] = None) -> requests.Response:
         url = self.url.format(token=self.token, method='sendMessage')
         payload = {'chat_id': chat_id, 'text': text}
+        if reply_markup:
+            payload |= {'reply_markup': reply_markup}
         response = requests.post(url, json=payload, timeout=10)
         response.raise_for_status()
         return response
--- league/tasks.py
+++ league/tasks.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import requests
 from celery import shared_task
 
@@ -10,11 +12,11 @@ from league import models, services
     retry_kwargs={'max_retries': 5},
     acks_late=True,
 )
-def send_telegram_message(player_id: int, text: str) -> None:
+def send_telegram_message(player_id: int, text: str, reply_markup: Optional[dict] = None) -> None:
     player = models.Player.objects.filter(id=player_id).first()
 
     if not player:
         return
 
     telegram_handler = services.TelegramHandler()
-    telegram_handler.send_message(chat_id=player.telegram_id, text=text)
+    telegram_handler.send_message(chat_id=player.telegram_id, text=text, reply_markup=reply_markup)
